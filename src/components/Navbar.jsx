dd842a7fb8a5b6e954515f2837249723fdcb0205
BirolAlkan <birolalkan93@gmail.com>
2022-12-02 16:28:13 +0300

    navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,49 @@
-import { NavLink } from "react-router-dom";
+import logo from "../img/alkan-logo.png";
+import { Link, NavLink } from "react-router-dom";
 
-const Navbar = () => {
+function Nav() {
   return (
-    <div className="d-flex justify-content-end gap-2 ">
-          <NavLink className="navbar-brand" to="#">
-//       <img src="./img/alkan-logo.png" alt="logo" />
-//     </NavLink>
-         <NavLink className="text-decoration-none text-dark" to="/" className="nav-link active" aria-current="page">
+    <nav className="navbar navbar-expand-md navbar-light">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand  ">
+          <img className="img-fluid" src={logo} alt="" />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 me-3 mb-lg-0">
+            <li className="nav-item">
+              <NavLink 
+              style={({ isActive }) => ({
+                color: isActive && "red",
+              })} 
+              to="/" className="nav-link active" aria-current="page">
                 Home
-         </NavLink>
-        
-        <NavLink className="text-decoration-none text-dark" to='/About'>About</NavLink> 
-        <NavLink className="text-decoration-none text-dark" to='/Contact'>Contact</NavLink> 
-        
-        
-    </div>     
-   
-  )
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <Link to="/about" className="nav-link" aria-current="page">
+                About
+              </Link>
+            </li>
+            
+            <li className="nav-item">
+              <Link to="/contact" className="nav-link" aria-current="page">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Nav;
